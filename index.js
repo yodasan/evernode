4210c1b6546fe06e9696f19bfe53e058cfa0cb64
Hiroki Kondo <kompiro@gmail.com>
2011-10-22 21:27:26 +0900

    packaging for npm

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.customConnections = require('./lib/evernode/CustomConnections'),
+exports.UserStore = require('./lib/evernote-thrift/gen-nodejs/UserStore')
+exports.NoteStore = require('./lib/evernote-thrift/gen-nodejs/NoteStore');
+
+exports.NoteStoreTypes = require('./lib/evernote-thrift/gen-nodejs/NoteStore_types');
