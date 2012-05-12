92b61ccf58bc2b5be29e97f93d0fe2df5fa96b21
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2012-05-12 19:23:11 +0700

    Revert "Revert "Add dependency modules""
    This reverts commit ec9dd4931f49bde166f34d33d900802a873f01d8.

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ exports.Evernode = require('./evernode');
 exports.customConnections = require('./lib/evernode/CustomConnections'),
 exports.UserStore = require('./lib/evernote-thrift/gen-nodejs/UserStore')
 exports.NoteStore = require('./lib/evernote-thrift/gen-nodejs/NoteStore');
-
 exports.NoteStoreTypes = require('./lib/evernote-thrift/gen-nodejs/NoteStore_types');
+
